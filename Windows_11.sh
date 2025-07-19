23aef4c5bd37a8ddafb9f4ca6c6bf4924ef00e00
Eskinder <eskinderget@gmail.com>
2025-07-19 16:01:49 +0300

    gpu

--- Windows_11.sh
+++ Windows_11.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-UUID="6a57a4b4-7e69-4038-98ae-5ca73979db06"
-
-# Create vGPU via sudo helper script
-sudo /usr/local/bin/manage-vgpu.sh create
-
 taskset -c 2,3 qemu-system-x86_64 \
   -enable-kvm \
   \
   -m 10G \
-  -object memory-backend-memfd,id=mem,size=10G,share=on \
-  -numa node,memdev=mem \
+  -mem-path /dev/myqemuhugepages \
+  -mem-prealloc \
+  \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/OVMF.fd \
+  -drive if=pflash,format=raw,file=/home/esk/Desktop/OV.fd \
   \
   -smp 2,sockets=1,dies=1,cores=2,threads=1 \
   -machine type=q35,accel=kvm,usb=off \
   -cpu host,+invtsc,+kvm_pv_unhalt,hv-time=on,hv-relaxed=on,hv-vapic=on,hv-spinlocks=0x1fff,hv-vpindex=on,hv-synic=on,hv-stimer=on,hv-stimer-direct=on,hv-reset=on,hv-frequencies=on,hv-reenlightenment=on,hv-tlbflush=on,hv-ipi=on \
   \
-  -drive file=/mnt/0ab1d5e2-3585-47a5-b39e-452f73aeac9c/Windows_11.qcow2,format=qcow2,if=virtio,cache=none,discard=unmap,aio=threads \
+  -drive file=/mnt/a16b6d0c-4275-466e-8378-0356bc49dcc4/Windows_11.qcow2,if=none,id=nvm \
+  -device nvme,drive=nvm,serial=qcow2Serial \
   \
-  -drive file=/mnt/57C4287151231A2D/ISO/virtio-win-0.1.266.iso,format=raw,if=none,media=cdrom,id=drive-cd1,readonly=on \
+  -drive file=/mnt/57C4287151231A2D/ISO/MICRO_WIN11.iso,format=raw,if=none,media=cdrom,id=drive-cd1,readonly=on \
   -device ahci,id=achi0 \
-  -device ide-cd,bus=achi0.0,drive=drive-cd1,id=cd1,bootindex=1 \
+  -device ide-cd,bus=achi0.0,drive=drive-cd1,id=cd1 \
+  -cdrom /mnt/57C4287151231A2D/ISO/virtio-win-0.1.266.iso \
+  \
+  -object memory-backend-file,id=ivshmem_mem,size=64M,share=on,mem-path=/dev/shm/looking-glass \
+  -device ivshmem-plain,memdev=ivshmem_mem \
   \
   -name Windows_11 \
   -rtc base=localtime,clock=host,driftfix=slew \
@@ -28,9 +31,10 @@ taskset -c 2,3 qemu-system-x86_64 \
   -device virtio-balloon-pci \
   -device usb-tablet \
   \
-  -device vfio-pci,sysfsdev=/sys/devices/pci0000:00/0000:00:02.0/${UUID},x-igd-opregion=on,display=on,driver=vfio-pci-nohotplug,ramfb=on \
   -vga none \
-  -display gtk,gl=on,window-close=off \
+  -device ramfb,id=ramfb0 \
+  -device vfio-pci,host=04:00.0,multifunction=on \
+  -device vfio-pci,host=04:00.1 \
   \
   -audiodev pipewire,id=snd0 \
   -device ich9-intel-hda \
@@ -46,5 +50,3 @@ taskset -c 2,3 qemu-system-x86_64 \
 QEMU_PID=$!
 
 wait $QEMU_PID
-# --- Remove vGPU after VM shuts down ---
-sudo /usr/local/bin/manage-vgpu.sh remove
