f624bc396e862395cc7146ad255f3accc482b9be
Eskinder <eskinderget@gmail.com>
2025-10-26 14:26:23 +0300

    windows 10 changes

--- Windows_10.sh
+++ Windows_10.sh
@@ -9,13 +9,12 @@ taskset -c 2,3 qemu-system-x86_64 \
   -enable-kvm \
   \
   -m 10G \
-  -object memory-backend-memfd,id=mem,size=10G,share=on \
+  -object memory-backend-memfd,id=mem,size=10G,share=on,prealloc=on \
   -numa node,memdev=mem \
   \
   -smp 2,sockets=1,dies=1,cores=2,threads=1 \
   -machine type=q35,accel=kvm,usb=off \
-  -cpu host,+invtsc,+kvm_pv_unhalt,hv-time=on,hv-relaxed=on,hv-vapic=on,hv-spinlocks=0x1fff,hv-vpindex=on,hv-synic=on,hv-stimer=on,hv-stimer-direct=on,hv-reset=on,hv-frequencies=on,hv-reenlightenment=on,hv-tlbflush=on,hv-ipi=on \
-  -global kvm-pit.lost_tick_policy=discard \
+  -cpu host,+kvm_pv_unhalt,hv-time=on,hv-relaxed=on,hv-vapic=on,hv-spinlocks=0x1fff,hv-vpindex=on,hv-synic=on,hv-stimer=on,hv-stimer-direct=on,hv-reset=on,hv-frequencies=on,hv-reenlightenment=on,hv-tlbflush=on,hv-ipi=on,hv-runtime=on,hv-passthrough=on \
   \
   -drive file=/home/esk/VMware/Windows_10.qcow2,format=qcow2,if=virtio,cache=none,discard=unmap,aio=threads \
   \
@@ -24,9 +23,8 @@ taskset -c 2,3 qemu-system-x86_64 \
   -device ide-cd,bus=achi0.0,drive=drive-cd1,id=cd1,bootindex=1 \
   \
   -name Windows_10 \
-  -rtc base=localtime,clock=host,driftfix=slew \
+  -rtc base=localtime \
   -usb \
-  -device virtio-balloon-pci \
   -device usb-tablet \
   \
   -display gtk,gl=on,window-close=off \
@@ -39,8 +37,7 @@ taskset -c 2,3 qemu-system-x86_64 \
   \
   -netdev bridge,id=net0,br=nm-bridge \
   -device virtio-net-pci,netdev=net0,mac=90:94:01:00:00:01 \
-  \
-  -object thread-context,id=tc1,cpu-affinity=2-3 \
+  -overcommit mem-lock=on \
   \
   "$@" &
 
