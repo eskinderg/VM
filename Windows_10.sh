afd439db966d79e4f1607d3df3e0bc49bb6a3534
Eskinder <eskinderget@gmail.com>
2026-01-15 17:03:13 +0300

    windows 10 changes

--- Windows_10.sh
+++ Windows_10.sh
@@ -2,21 +2,32 @@
 
 UUID="6a57a4b4-7e69-4038-98ae-5ca73979db06"
 
+# 1. Clear caches to free up "easy" RAM
+sync && sudo sysctl -w vm.drop_caches=3
+
+# 2. Force the kernel to compact memory into contiguous blocks
+sudo sysctl -w vm.compact_memory=1
+
+# 3. Allocate the HugePages
+sudo sysctl -w vm.nr_hugepages=5120
+
 # Create vGPU via sudo helper script
 sudo /usr/local/bin/manage-vgpu.sh create
 
-  nice -n -15 taskset -c 2,3 qemu-system-x86_64 \
+nice -n -15 taskset -c 2,3 qemu-system-x86_64 \
   -enable-kvm \
   \
   -m 10G \
-  -object memory-backend-memfd,id=mem,size=10G,share=on,prealloc=on \
+  -object memory-backend-memfd,id=mem,size=10G,share=off,prealloc=on,hugetlb=on \
   -numa node,memdev=mem \
   \
   -smp 2,sockets=1,dies=1,cores=2,threads=1 \
   -machine type=q35,accel=kvm,usb=off,hpet=off \
   -cpu host,+kvm_pv_unhalt,hv-time=on,hv-relaxed=on,hv-vapic=on,hv-spinlocks=0x1fff,hv-vpindex=on,hv-synic=on,hv-stimer=on,hv-stimer-direct=on,hv-reset=on,hv-frequencies=on,hv-reenlightenment=on,hv-tlbflush=on,hv-ipi=on,hv-runtime=on,hv-passthrough=on \
   \
-  -drive file=/home/esk/VMware/Windows_10.qcow2,format=qcow2,if=virtio,cache=none,discard=unmap,aio=threads \
+  -object iothread,id=iothread0,poll-max-ns=0 \
+  -drive file=/home/esk/VMware/Windows_10.qcow2,if=none,id=drive0,cache=none,aio=native,discard=unmap,detect-zeroes=unmap \
+  -device virtio-blk-pci,drive=drive0,iothread=iothread0,num-queues=2 \
   \
   -drive file=/mnt/57C4287151231A2D/ISO/virtio-win-0.1.266.iso,format=raw,if=none,media=cdrom,id=drive-cd1,readonly=on \
   -device ahci,id=achi0 \
@@ -35,7 +46,7 @@ sudo /usr/local/bin/manage-vgpu.sh create
   -device ich9-intel-hda -device hda-duplex,audiodev=snd0 \
   \
   -netdev bridge,id=net0,br=nm-bridge \
-  -device virtio-net-pci,netdev=net0,mac=90:94:01:00:00:01 \
+  -device virtio-net-pci,netdev=net0,mac=90:94:01:00:00:01,mq=on,vectors=8 \
   -overcommit mem-lock=on \
   \
   -global kvm-pit.lost_tick_policy=delay \
@@ -46,3 +57,5 @@ QEMU_PID=$!
 wait $QEMU_PID
 # --- Remove vGPU after VM shuts down ---
 sudo /usr/local/bin/manage-vgpu.sh remove
+
+sudo sysctl -w vm.nr_hugepages=0
